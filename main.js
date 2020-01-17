4683444dc3593362518f32bd45c6f9d2e561bf47
Matt Reeds <mattlreeds@gmail.com>
2020-01-17 14:28:07 -0600

    some preogress made on loop-not much

---
+++ main.js
@@ -0,0 +1,13 @@
+let bandNumber = 1
+
+const takeNumber = function () {
+    for ( i=0; i < bandNumber.valueOf; i++)
+    return (`${takeNumber} + ${bandNumber}`)
+}
+takeNumber()
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
